95d8f97159e03e4dc53a3f5182868abc590b40b9
vanill4Sky <wojtek-s22@wp.pl>
2020-11-04 17:05:52 +0100

    Remove Random class
    Ta klasa jest w sumie teraz niepotrzebna, bo korzystamy z <random>.

--- gpu-matrix-sort/src/random.cpp
+++
@@ -1,7 +0,0 @@
-#include "random.h"
-#include <stdlib.h>
-
-int Random::getRand(int a, int b)
-{
-	return rand() % (b - a + 1) + a;
-}
--- gpu-matrix-sort/src/random.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Random
-{
-	// class for getting random values from given range
-public:
-	int getRand(int a, int b);
-};
-
